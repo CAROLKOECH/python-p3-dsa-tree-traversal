ca0ba32f3337f34b04bbb2a9475c21c323ad831b
CAROLKOECH <koechc44@gmail.com>
2023-09-01 21:08:34 +0300

    done

--- lib/testing/get_element_by_id_test.py
+++ lib/testing/get_element_by_id_test.py
@@ -1,48 +1,42 @@
-from tree import Tree
 
-class TestStack:
-    '''Class Stack in Stack.py'''
+from lib.tree import Tree
 
-    def test_get_element_by_id(self):
-  # <body>
-  #   <div id="main">
-  #     <h1 id="heading">Title</h1>
-  #     <h2>Subtitle</h2>
-  #   </div>
-  # </body>
-
-        '''get_element_by_id test'''
-    tree = Tree(
-      {
+def test_get_element_by_id():
+    # Create the tree structure
+    tree = Tree({
         'tag_name': 'body',
         'id': None,
         'children': [
-          {
-            'tag_name': 'div',
-            'id': 'main',
-            'children': [
-              {
-                'tag_name': 'h1',
-                'id': 'heading',
-                'text_content': 'Title',
-                'children': []
-              },
-              {
-                'tag_name': 'h2',
-                'id': None,
-                'text_content': 'Subitle',
-                'children': []
-              }
-            ]
-          }
+            {
+                'tag_name': 'div',
+                'id': 'main',
+                'children': [
+                    {
+                        'tag_name': 'h1',
+                        'id': 'heading',
+                        'text_content': 'Title',
+                        'children': []
+                    },
+                    {
+                        'tag_name': 'h2',
+                        'id': None,
+                        'text_content': 'Subtitle',
+                        'children': []
+                    }
+                ]
+            }
         ]
-      }
-    )
-    assert(tree.get_element_by_id('heading') =={
-      'tag_name': 'h1',
-      'id': 'heading',
-      'text_content': 'Title',
-      'children': []
-    } )
+    })
 
-    assert(tree.get_element_by_id('nope') == None)
+    # Test cases
+    assert tree.get_element_by_id('heading') == {
+        'tag_name': 'h1',
+        'id': 'heading',
+        'text_content': 'Title',
+        'children': []
+    }
+
+    assert tree.get_element_by_id('nope') is None
+
+if __name__ == '__main__':
+    test_get_element_by_id()
